f2d0af63d96397c1d5bc146ebcc678adf257ee8e
Dipinjit Hanspal <13964659+DipinjitHanspal@users.noreply.github.com>
2019-12-30 19:25:59 -0500

    Merge working state of VueJS frontend into backend (#1)
    * Add Vue cli
    
    * Baseline install before vuejs
    
    * Added scaffolding for vuejs
    
    * VueJS in a working state

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Crud;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
